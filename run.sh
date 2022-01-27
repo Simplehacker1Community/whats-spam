9e5839240a2c052d86e9e705f76bd38668248dae
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-01-27 22:02:46 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#==============================================================>
+#==============================================================>
+#||   Auther : kathakali and Learn cyber security Group      ||
+#||   Date :  30/12/2021                                     ||
+#||   Update : 27/01/2022                                    ||
+#||   Version : 1.2.0 beta                                   ||
+#||   About : Termux Spam Messaging                          ||
+#==============================================================>
+#==============================================================>
+
+wget -q  https://raw.githubusercontent.com/Simplehacker1Community/whats-spam/simplehacker/.ping &> /div/null 
+if grep -q truelove .ping
+then
+ echo updateing 
+ rm setup.sh
+ wget -q  https://raw.githubusercontent.com/Simplehacker1Community/whats-spam/simplehacker/setup.sh &> /div/null 
+ clear
+ bash setup.sh
+ exit
+fi
+python main.py
