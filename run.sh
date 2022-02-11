c331de9c2352db1824a6ea28f378ea7d99f5a861
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-02-11 19:42:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 #==============================================================>
 #||   Auther : kathakali and Learn cyber security Group      ||
 #||   Date :  30/12/2021                                     ||
-#||   Update : 27/01/2022                                    ||
-#||   Version : 1.2.0 beta                                   ||
+#||   Update : 11/02/2022                                    ||
+#||   Version : 1.3.0 relese                                  ||
 #||   About : Termux Spam Messaging                          ||
 #==============================================================>
 #==============================================================>
 rm .ping
 wget -q  https://raw.githubusercontent.com/Simplehacker1Community/whats-spam/simplehacker/.ping &> /div/null 
-if grep -q truelove2 .ping
+if grep -q truelove3 .ping
 then
  echo updateing 
  rm setup.sh
