844e5448430649867d997836670ae5f5074a3317
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-02-22 15:48:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ apt update  && apt upgrade -y
 clear
 apt install python wget curl git -y
 pip install --upgrade whatsspam
+pip install --upgrade requests
 git clone https://github.com/Simplehacker1Community/whats-spam
 cd whats-spam
 chmod +x *
